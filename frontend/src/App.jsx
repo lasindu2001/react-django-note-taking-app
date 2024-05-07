975d22f35691f0ccbbfeb2056ba3522d02d23808
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-05-07 15:44:59 +0530

    create navigation and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,43 @@
 import React from "react"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import Register from "./pages/Register"
+import ProtectedRoute from "./components/ProtectedRoute"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import NotFound from "./pages/NotFound"
+
+function Logout() {
+  localStorage.clear()
+  return (
+    <Navigate to="/login" />
+  )
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return (
+    <Register />
+  )
+}
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />}/>
+        <Route path="/register" element={<RegisterAndLogout />}/>
+        <Route path="*" element={<NotFound />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
